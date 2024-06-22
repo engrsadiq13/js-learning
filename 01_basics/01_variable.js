d5e6440a2d1f007eae89f7575dbb09b289d07bad
Engr. Sadiq Amin <87617398+engrsadiq13@users.noreply.github.com>
2024-06-22 22:34:24 +0500

    usage of variables keywords

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "engrsadiq13@gmail.com"
+let accountPass = "hello"
+let accountCity = "Bannu"
+
+// var is not recommended for usage because issue occures in block scop and functional scope
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity]);
