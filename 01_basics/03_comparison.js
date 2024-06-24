0f50d4a18845a22ebb3f1d61ad0f4a6888aa72bc
Engr. Sadiq Amin <87617398+engrsadiq13@users.noreply.github.com>
2024-06-24 08:58:34 +0500

    operations with string

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1)
+// console.log(2==1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log("2"<=1)
+// // in javascript we use === for more in depth conversion as it compares the data types also
+// console.log("2"===2)
+// console.log( 2===2)
+// *************operations**********************
+
+let str1 = "hello"
+let str2 = " Sadiq"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);//results in 122 because the first value is string so it considers all as a string
+// console.log(1 + 2 + "2"); // results in 32 as if operation is first then it will be performed first
+let gamCounter = 100
+gamCounter++;
+console.log(gamCounter);
