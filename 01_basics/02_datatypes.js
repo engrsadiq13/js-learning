3766c2259f65319a01700d721a6c783c959e1cf0
Engr. Sadiq Amin <87617398+engrsadiq13@users.noreply.github.com>
2024-06-24 08:08:05 +0500

    data type conversion

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,17 @@
 //numbers, string, bigInt, boolean, symbol
+let myNumber = 33
+let myText = "Sadiq"
+let myBooleanisLoggedIn = true
+let myUndefined = undefined
+let myNull = null
+
+// console.log(typeof myNumber);
+// console.log(typeof myText);
+// console.log(typeof myBooleanisLoggedIn);
+// console.log(typeof myUndefined);
+// console.log(typeof myNull);
+
+// let myNumberconvert = Number(myText) // convert any type of data to Number
+// console.log(typeof myNumberconvert);
+// console.log(myNumberconvert);
+// console.log(myUndefined)
