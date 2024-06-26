06b296849687150ec17c27609d3be33f7bc46e16
Engr. Sadiq Amin <87617398+engrsadiq13@users.noreply.github.com>
2024-06-26 16:10:20 +0500

    Dates in JS

---
+++ 01_basics/06_datesinjs.js
@@ -0,0 +1,13 @@
+//dates
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleString());
+
+let mycreatedDate = new Date("01-14-2023")
+// console.log(mycreatedDate.getTime());
+
+let myStamp = Date.now()
+console.log(Math.floor(myStamp/1000));
